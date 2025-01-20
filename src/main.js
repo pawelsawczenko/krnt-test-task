31441836633d6b904aaef2f8e6d04434b448926e
pawelsawczenko <pawelsawczenko@gmail.com>
2025-01-21 00:14:46 +0300

    add header

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "normalize.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const hamMenuBtn = document.querySelector(".header__hamburger-btn");
+const menu = document.querySelector(".header__menu-wrapper");
+const closeMenuBtn = document.querySelector(".header__close-btn");
 
-setupCounter(document.querySelector('#counter'))
+const toggleMenu = () => {
+  menu.classList.toggle("header__menu-wrapper_hidden");
+  menu.classList.toggle("header__menu-wrapper_visible");
+};
+
+hamMenuBtn.addEventListener("click", toggleMenu);
+
+closeMenuBtn.addEventListener("click", toggleMenu);
